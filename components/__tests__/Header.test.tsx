1d9db3fa4d84a31d8a37eaad1d4d1053c3d2c4ae
Yirer <xeroonelove@gmail.com>
2019-06-16 18:19:46 +0900

    init config

---
+++ components/__tests__/Header.test.tsx
@@ -0,0 +1,12 @@
+/* eslint-env jest */
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Header from '../Header';
+
+describe('Header', () => {
+  it('renders the h1 title', () => {
+    const header = shallow(<Header />);
+    expect(header.find('a'));
+  });
+});
